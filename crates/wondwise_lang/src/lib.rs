58acb2dcdcb97b4a79c09d7290a8204134e1bdb2
Jheyson Saavedra <jheysonsaav@users.noreply.github.com>
2021-04-29 16:13:25 -0500

    feat(wondwise_lang): add token and tests

--- crates/wondwise_lang/src/lib.rs
+++ crates/wondwise_lang/src/lib.rs
@@ -1,8 +1,3 @@
 // Copyright 2021 Wondwise Authors. All rights reserved. GPL-3.0 license.
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+
+mod token;
