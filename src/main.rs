edc051ecb26df1035958135491bfbfa1d880fb32
Jheyson Saavedra <jheysonsaav@users.noreply.github.com>
2021-05-03 15:52:52 -0500

    feat: add utils

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
-use clap::{crate_description, crate_version, App};
+mod utils;
+
+use crate::utils::logs::{Log, LogLevel};
+use clap::{crate_description, crate_version, App, Arg};
 use rustyline::{error::ReadlineError, Editor};
+use std::env;
+use utils::dirs::WondwiseDirs;
 
 fn main() {
     // Create clap application
-    let _matches = App::new("wondwise")
+    let matches = App::new("wondwise")
         .version(crate_version!())
         .about(crate_description!())
+        .arg(
+            Arg::with_name("log-level")
+                .short("L")
+                .long("log-level")
+                .default_value("info")
+                .possible_values(&["info", "debug"])
+                .takes_value(true)
+                .global(true),
+        )
         .get_matches();
+
+    env::set_var("LOG_LEVEL", matches.value_of("log-level").unwrap());
 
     // Create and start prompt with rustyline
     let mut rl = Editor::<()>::new();
-    if rl.load_history("history.txt").is_err() {
-        println!("No previous history.");
+
+    #[cfg(unix)]
+    let history_file = format!(
+        "{}/history.yml",
+        WondwiseDirs::load().data_dir().to_str().unwrap()
+    );
+
+    #[cfg(windows)]
+    let history_file = format!(
+        "{}\\history.yml",
+        WondwiseDirs::load().data_dir().as_str().unwrap()
+    );
+
+    if rl.load_history(history_file.as_str()).is_err() {
+        Log::new(LogLevel::Debug, 2, "No previous history").show();
     }
     loop {
         let readline = rl.readline("-| ");
@@ -31,5 +60,5 @@ fn main() {
             }
         }
     }
-    rl.save_history("history.txt").unwrap();
+    rl.save_history(history_file.as_str()).unwrap();
 }
